cff8f37f2dfb3f5e43d8d23f996018a6c4174d0f
Ccshl <liucong@henu.edu.cn>
2025-05-30 15:53:11 +0800

    STMC_headUNet.py

--- STMC_headUNet.py
+++ STMC_headUNet.py
@@ -27,7 +27,6 @@ def gcd(a, b):
     return a
 
 
-# Other types of layers can go here (e.g., nn.Linear, etc.)
 def _init_weights(module, name, scheme=''):
     if isinstance(module, nn.Conv2d) or isinstance(module, nn.Conv3d):
         if scheme == 'normal':
@@ -62,7 +61,6 @@ def _init_weights(module, name, scheme=''):
 
 
 def act_layer(act, inplace=False, neg_slope=0.2, n_prelu=1):
-    # activation layer
     act = act.lower()
     if act == 'relu':
         layer = nn.ReLU(inplace)
@@ -118,22 +116,16 @@ class MSDC(nn.Module):
         named_apply(partial(_init_weights, scheme=scheme), self)
 
     def forward(self, x):
-        # Apply the convolution layers in a loop
         outputs = []
         for dwconv in self.dwconvs:
             dw_out = dwconv(x)
             outputs.append(dw_out)
             if self.dw_parallel == False:
                 x = x + dw_out
-        # You can return outputs based on what you intend to do with them
         return outputs
 
 
 class MSCB(nn.Module):
-    """
-    Multi-scale convolution block (MSCB)
-    """
-
     def __init__(self, in_channels, out_channels, stride, kernel_sizes=[1, 3, 5], expansion_factor=2, dw_parallel=True,
                  add=True, activation='relu6'):
         super(MSCB, self).__init__()
@@ -147,15 +139,11 @@ class MSCB(nn.Module):
         self.add = add
         self.activation = activation
         self.n_scales = len(self.kernel_sizes)
-        # check stride value
         assert self.stride in [1, 2]
-        # Skip connection if stride is 1
         self.use_skip_connection = True if self.stride == 1 else False
 
-        # expansion factor
         self.ex_channels = int(self.in_channels * self.expansion_factor)
         self.pconv1 = nn.Sequential(
-            # pointwise convolution
             nn.Conv2d(self.in_channels, self.ex_channels, 1, 1, 0, bias=False),
             nn.BatchNorm2d(self.ex_channels),
             act_layer(self.activation, inplace=True)
@@ -167,7 +155,6 @@ class MSCB(nn.Module):
         else:
             self.combined_channels = self.ex_channels * self.n_scales
         self.pconv2 = nn.Sequential(
-            # pointwise convolution
             nn.Conv2d(self.combined_channels, self.out_channels, 1, 1, 0, bias=False),
             nn.BatchNorm2d(self.out_channels),
         )
@@ -197,12 +184,8 @@ class MSCB(nn.Module):
             return out
 
 
-#   Multi-scale convolution block (MSCB)
 def MSCBLayer(in_channels, out_channels, n=1, stride=1, kernel_sizes=[1, 3, 5], expansion_factor=2, dw_parallel=True,
               add=True, activation='relu6'):
-    """
-    create a series of multi-scale convolution blocks.
-    """
     convs = []
     mscb = MSCB(in_channels, out_channels, stride, kernel_sizes=kernel_sizes, expansion_factor=expansion_factor,
                 dw_parallel=dw_parallel, add=add, activation=activation)
@@ -216,7 +199,6 @@ def MSCBLayer(in_channels, out_channels, n=1, stride=1, kernel_sizes=[1, 3, 5], 
     return conv
 
 
-#   Channel attention block (CAB)
 class CAB(nn.Module):
     def __init__(self, in_channels, out_channels=None, ratio=16, activation='relu'):
         super(CAB, self).__init__()
@@ -252,9 +234,6 @@ class CAB(nn.Module):
         out = avg_out + max_out
         return self.sigmoid(out)
 
-    #   Spatial attention block (SAB)
-
-
 class SAB(nn.Module):
     def __init__(self, kernel_size=7):
         super(SAB, self).__init__()
@@ -280,35 +259,6 @@ class SAB(nn.Module):
 
 
 class BasicConvBlock(nn.Module):
-    """Basic convolutional block for UNet.
-
-    This module consists of several plain convolutional layers.
-
-    Args:
-        in_channels (int): Number of input channels.
-        out_channels (int): Number of output channels.
-        num_convs (int): Number of convolutional layers. Default: 2.
-        stride (int): Whether use stride convolution to downsample
-            the input feature map. If stride=2, it only uses stride convolution
-            in the first convolutional layer to downsample the input feature
-            map. Options are 1 or 2. Default: 1.
-        dilation (int): Whether use dilated convolution to expand the
-            receptive field. Set dilation rate of each convolutional layer and
-            the dilation rate of the first convolutional layer is always 1.
-            Default: 1.
-        with_cp (bool): Use checkpoint or not. Using checkpoint will save some
-            memory while slowing down the training speed. Default: False.
-        conv_cfg (dict | None): Config dict for convolution layer.
-            Default: None.
-        norm_cfg (dict | None): Config dict for normalization layer.
-            Default: dict(type='BN').
-        act_cfg (dict | None): Config dict for activation layer in ConvModule.
-            Default: dict(type='ReLU').
-        dcn (bool): Use deformable convolution in convolutional layer or not.
-            Default: None.
-        plugins (dict): plugins for convolutional layers. Default: None.
-    """
-
     def __init__(self,
                  in_channels,
                  out_channels,
@@ -343,8 +293,6 @@ class BasicConvBlock(nn.Module):
         self.convs = nn.Sequential(*convs)
 
     def forward(self, x):
-        """Forward function."""
-
         if self.with_cp and x.requires_grad:
             out = cp.checkpoint(self.convs, x)
         else:
@@ -369,55 +317,31 @@ class unetUp(nn.Module):
 
 class EMA(nn.Module):
     def __init__(self, channels, c2=None, factor=32):
-        # 调用父类的构造函数。
         super(EMA, self).__init__()
-        # 将通道数分成多个组，组的数量为 factor。
         self.groups = factor
-        # 确保每组至少有一个通道。
         assert channels // self.groups > 0
-        # 定义一个 softmax 层，用于计算权重。
         self.softmax = nn.Softmax(-1)
-        # 自适应平均池化，将每个通道缩放到 (1,1) 的输出。
         self.agp = nn.AdaptiveAvgPool2d((1, 1))
-        # 自适应平均池化，将高维度缩放到 1，宽维度保持不变。
         self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
-        # 自适应平均池化，将宽维度缩放到 1，高维度保持不变。
         self.pool_w = nn.AdaptiveAvgPool2d((1, None))
-        # 组归一化，每组的通道数为 channels // groups。
         self.gn = nn.GroupNorm(channels // self.groups, channels // self.groups)
-        # 1x1 卷积层，用于通道的转换和维度缩减。
         self.conv1x1 = nn.Conv2d(channels // self.groups, channels // self.groups, kernel_size=1, stride=1, padding=0)
-        # 3x3 卷积层，用于提取局部特征。
         self.conv3x3 = nn.Conv2d(channels // self.groups, channels // self.groups, kernel_size=3, stride=1, padding=1)
 
     def forward(self, x):
-        # 获取输入 x 的形状 (batch_size, channels, height, width)。
         b, c, h, w = x.size()
-        # 将输入 x 重新排列为 (batch_size * groups, channels // groups, height, width)。
         group_x = x.reshape(b * self.groups, -1, h, w)
-        # 计算沿高度方向的池化，得到大小为 (batch_size * groups, channels // groups, height, 1) 的张量。
         x_h = self.pool_h(group_x)
-        # 计算沿宽度方向的池化，得到大小为 (batch_size * groups, channels // groups, 1, width) 的张量，并进行转置。
         x_w = self.pool_w(group_x).permute(0, 1, 3, 2)
-        # 将两个池化的张量连接在一起，并通过 1x1 卷积层，得到一个新的特征图。
         hw = self.conv1x1(torch.cat([x_h, x_w], dim=2))
-        # 将特征图按原来的高度和宽度切分，分别得到 x_h 和 x_w。
         x_h, x_w = torch.split(hw, [h, w], dim=2)
-        # 使用 sigmoid 激活函数和 x_h, x_w 调整 group_x 的特征值。
         x1 = self.gn(group_x * x_h.sigmoid() * x_w.permute(0, 1, 3, 2).sigmoid())
-        # 使用 3x3 卷积层对 group_x 进行特征提取。
         x2 = self.conv3x3(group_x)
-        # 计算 x1 的平均池化并通过 softmax，得到权重。
         x11 = self.softmax(self.agp(x1).reshape(b * self.groups, -1, 1).permute(0, 2, 1))
-        # 将 x2 重新排列为 (batch_size * groups, channels // groups, height * width) 的形状。
         x12 = x2.reshape(b * self.groups, c // self.groups, -1)
-        # 计算 x2 的平均池化并通过 softmax，得到权重。
         x21 = self.softmax(self.agp(x2).reshape(b * self.groups, -1, 1).permute(0, 2, 1))
-        # 将 x1 重新排列为 (batch_size * groups, channels // groups, height * width) 的形状。
         x22 = x1.reshape(b * self.groups, c // self.groups, -1)
-        # 计算 x11 和 x12, x21 和 x22 的矩阵乘法，并将结果 reshape 为 (batch_size * groups, 1, height, width)。
         weights = (torch.matmul(x11, x12) + torch.matmul(x21, x22)).reshape(b * self.groups, 1, h, w)
-        # 使用权重调整 group_x 的特征，并 reshape 为原始的形状 (batch_size, channels, height, width)。
         return (group_x * weights.sigmoid()).reshape(b, c, h, w)
 
 
@@ -599,7 +523,7 @@ class UNetMEMCAMYFSKIPHead(BaseDecodeHead):
         f11 = f11 + feat1
 
 
-        # MSCAM4
+
         da4 = self.cab4(f44)
         db4 = self.mscb4(f44)
         d4 = da4 * db4
@@ -620,7 +544,7 @@ class UNetMEMCAMYFSKIPHead(BaseDecodeHead):
         d3 = torch.cat([ft3, dt3], dim=1)
         d3 = self.conv1(d3)
 
-        # MSCAM3
+
         da3 = self.cab3(d3)
         db3 = self.mscb3(d3)
         d3 = da3 * db3
@@ -641,7 +565,6 @@ class UNetMEMCAMYFSKIPHead(BaseDecodeHead):
         d2 = torch.cat([ft2, dt2], dim=1)
         d2 = self.conv2(d2)
 
-        # MSCAM2
         da2 = self.cab2(d2)
         db2 = self.mscb2(d2)
         d2 = da2 * db2
@@ -662,7 +585,7 @@ class UNetMEMCAMYFSKIPHead(BaseDecodeHead):
         d1 = torch.cat([ft1, dt1], dim=1)
         d1 = self.conv3(d1)
 
-        # MSCAM1
+
         da1 = self.cab1(d1)
         db1 = self.mscb1(d1)
         d1 = da1 * db1
